6100bda584913c4a2ee4cb9a421be36d3eb6288b
cvellle <cuekisa@yahoo.com>
2024-10-25 00:52:13 +0200

    server js

--- index.ts
+++ index.ts
@@ -67,4 +67,4 @@ app.use(errorHandler);
 //   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // });
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-// module.exports = app;
+module.exports = app;
