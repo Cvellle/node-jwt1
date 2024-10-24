69f273b0595258d1a33a96e5ec877e9ebadd4a96
cvellle <cuekisa@yahoo.com>
2024-10-25 00:40:26 +0200

    vercel json

--- index.ts
+++ index.ts
@@ -62,9 +62,9 @@ app.all("*", (req, res) => {
 
 app.use(errorHandler);
 
-mongoose.connection.once("open", () => {
-  console.log("Connected to MongoDB");
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-});
-
-module.exports = app;
+// mongoose.connection.once("open", () => {
+//   console.log("Connected to MongoDB");
+//   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// });
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// module.exports = app;
