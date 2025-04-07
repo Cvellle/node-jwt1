00b6afa07b3fff309b9c2be812b0da8f78b840c6
cvellle <cuekisa@yahoo.com>
2025-04-07 14:34:52 +0200

    route

--- api/routes/userRoutes.ts
+++ api/routes/userRoutes.ts
@@ -4,6 +4,6 @@ import { createUser, signinUser } from '../controllers/userControllers';
 const router = Router();
 
 router.post('/auth/user/signup', createUser);
-router.post('/auth/user/signin', signinUser);
+router.post('/auth/signin', signinUser);
 
 export default router;
