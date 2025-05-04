caba9e0c606f0ecad835bd512855cb7a575f8508
cvellle <cuekisa@yahoo.com>
2025-05-04 14:15:58 +0200

    mail

--- api/controllers/userControllers.ts
+++ api/controllers/userControllers.ts
@@ -5,20 +5,23 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 export const signinUser = async (req, res) => {
-    const { user, pwd } = req.body;
-    if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
+    const { username, pwd } = req.body;
+    console.log(123, req.body)
+    if (!username || !username) return res.status(400).json({ 'message': 'Username and password are required.' });
 
-    const foundUser = await User.findOne({ username: user }).exec();
+    const foundUser = await User.findOne({ username: username }).exec();
+    console.log(223, foundUser);
+    
     if (!foundUser) return res.sendStatus(401); //Unauthorized 
     // evaluate password 
-    const match = await bcrypt.compare(pwd, foundUser.password);
+    const match = await bcrypt.compare(pwd, foundUser.username);
     if (match) {
         const roles = Object.values(foundUser.roles).filter(Boolean);
         // create JWTs
         const accessToken = jwt.sign(
             {
                 "UserInfo": {
-                    "email": foundUser.email,
+                    "username": foundUser.username,
                     "roles": roles
                 }
             },
@@ -26,15 +29,13 @@ export const signinUser = async (req, res) => {
             { expiresIn: '10s' }
         );
         const refreshToken = jwt.sign(
-            { "email": foundUser.email },
+            { "username": foundUser.username },
             process.env.REFRESH_TOKEN_SECRET,
             { expiresIn: '1d' }
         );
         // Saving refreshToken with current user
         foundUser.refreshToken = refreshToken;
         const result = await foundUser.save();
-        console.log(result);
-        console.log(roles);
 
         // Creates Secure Cookie with refresh token
         res.cookie('jwt', refreshToken, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 });
