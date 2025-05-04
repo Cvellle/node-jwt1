4276a4c869a1b050b4439d01690d8f75c4493fbf
cvellle <cuekisa@yahoo.com>
2025-05-04 14:36:31 +0200

    mail

--- api/controllers/userControllers.ts
+++ api/controllers/userControllers.ts
@@ -13,15 +13,15 @@ export const signinUser = async (req, res) => {
     console.log(223, foundUser);
     
     if (!foundUser) return res.sendStatus(401); //Unauthorized 
-    // evaluate password 
-    const match = await bcrypt.compare(password, foundUser.username);
+    // evaluate password
+    const match = await bcrypt.compare(password, foundUser.password);
     if (match) {
         const roles = Object.values(foundUser.roles).filter(Boolean);
         // create JWTs
         const accessToken = jwt.sign(
             {
                 "UserInfo": {
-                    "email": foundUser.email,
+                    "username": foundUser.username,
                     "roles": roles
                 }
             },
@@ -29,7 +29,7 @@ export const signinUser = async (req, res) => {
             { expiresIn: '10s' }
         );
         const refreshToken = jwt.sign(
-            { "email": foundUser.email },
+            { "username": foundUser.username },
             process.env.REFRESH_TOKEN_SECRET,
             { expiresIn: '1d' }
         );
@@ -39,7 +39,7 @@ export const signinUser = async (req, res) => {
         // original
         // const result = await foundUser.save();
         await foundUser.save();
-
+        // res.status(201).json(foundUser);
         // Creates Secure Cookie with refresh token
         res.cookie('jwt', refreshToken, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 });
 
